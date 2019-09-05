844911221b936fcb907482d0444830239b23931d
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-09-05 15:35:48 -0700

    cards displayed

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,10 +2,20 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios.get("https://api.github.com/users/arizephyr123")
-.then(response =>{
-  console.log(response);
+
+const cards = document.querySelector('.cards');
+console.log(cards);
+
+axios
+.get("https://api.github.com/users/arizephyr123")
+.then(response => {
+  console.log(response.data);
+  CreateCard(response);
 })
+.catch(error => {
+  console.log("The GitHub data was not returned", error);
+});
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -28,7 +38,26 @@ axios.get("https://api.github.com/users/arizephyr123")
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"];
+
+  followersArray.forEach(item =>{
+    let followerUrl = `https://api.github.com/users/${item}`;
+    console.log(followerUrl);
+    axios
+.get(followerUrl)
+.then(response => {
+  console.log(response.data);
+  CreateCard(response);
+})
+.catch(error => {
+  console.log("The GitHub data was not returned", error);
+});
+  })
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -47,8 +76,57 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
+
+
+function CreateCard(obj) {
+  //create elements
+  const userCard = document.createElement("div"),
+    userImg = document.createElement("img"),
+    infoDiv = document.createElement("div"),
+    name = document.createElement("h3"),
+    username = document.createElement("p"),
+    location = document.createElement("p"),
+    profile = document.createElement("p"),
+    profileLink = document.createElement("a"),
+    followers = document.createElement("p"),
+    following = document.createElement("p"),
+    bio = document.createElement("p");
+
+  //create structure
+  cards.appendChild(userCard);
+  userCard.appendChild(userImg);
+  userCard.appendChild(infoDiv);
+  infoDiv.appendChild(name);
+  infoDiv.appendChild(username);
+  infoDiv.appendChild(location);
+  infoDiv.appendChild(profile);
+  infoDiv.appendChild(profileLink);
+  infoDiv.appendChild(followers);
+  infoDiv.appendChild(following);
+  infoDiv.appendChild(bio);
+
+  //assign classes
+  userCard.classList.add("card");
+  infoDiv.classList.add("card-info");
+  name.classList.add("name");
+  username.classList.add("username");
+
+  //set textContent
+  userImg.textContent = obj.data.avatar_url;
+  name.textContent = obj.data.name;
+  username.textContent = obj.data.login;
+  location.textContent = obj.data.location;
+  //profile.textContent = obj.data.===> need this??
+  profileLink.textContent = obj.data.html_url;
+  followers.textContent = obj.data.followers;
+  following.textContent = obj.data.following;
+  bio.textContent = obj.data.bio;
+
+  
+
+  return userCard
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
